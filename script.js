98e6c1d0b5c7686413960b4dd3b4a31bd30304da
007jbks <99896785+007jbks@users.noreply.github.com>
2024-12-22 14:53:28 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
-function place(){
-     
+for(const card of document.querySelectorAll(".card")){
+    card.onmousemove= e => handleOnMouseMove(e);
+}
+
+document.getElementById("cards").onmousemove = e => {
+    for(const card of document.getElementsByClassName("card")){
+        const rect = card.getBoundingClientRect();
+        const x= e.clientX - rect.left;
+        const y= e.clientY - rect.top;
+        card.style.setProperty("--mouse-x", `${x}px`);
+        card.style.setProperty("--mouse-y", `${y}px`);
+    }
 }
